7fedb606a5ee48d5fe1322b4aaf2fe3d7002fbad
swastik sharma <swastik.343@gmail.com>
2023-03-03 10:33:54 +0530

    added api data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
 import "./App.css";
 import { readMessage, decrypt, encrypt } from "openpgp";
+import { uuid } from "uuidv4";
 import useState from "react";
+const apiKey =
+  "QVBJX0tFWTo4NWY2M2I0MWZiNDkwMjA0NTFjZjUzMjE2OGYyMzUxMzo1ODdhZmMwMTU3NmRlMjY1OGYyZDFmYTU1Yjk0ODQxMA";
+
+const getKeyUrl = "";
+const postDataUrl = "";
 
 function App() {
   const [data, setData] = useState(null);
 
-  async function postData() {
-    const response = await fetch(url, {
-      method: "POST",
+  async function getKey() {
+    const response = await fetch(getKeyUrl, {
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
+    });
+    const res = await response.json();
+    if (res) {
+      console.log(res);
+      const publicKey = "";
+      // postData(publicKey)
+    }
+  }
+
+  async function postData(publicKey) {
+    const cardNumber = 4200000000000000;
+    const cvv = 123;
+
+    const cardDetails = {
+      number: cardNumber.trim().replace(/\D/g, ""),
+      cvv,
+    };
+    const encryptedData = await encrypt(cardDetails);
+
+    const reqData = {
+      idempotencyKey: uuid(),
+      expMonth: 1,
+      expYear: 2025,
+      keyId: publicKey,
+      encryptedData: encryptedData,
+      billingDetails: {
+        name: "Customer 0004",
+        country: "US",
+        district: "MA",
+        line1: "Test",
+        line2: "",
+        city: "Test City",
+        postalCode: "11111",
+      },
+      metadata: {
+        email: "customer-0004@circle.com",
+        phoneNumber: "+12025550180",
+        sessionId: "xxx",
+        ipAddress: "172.33.222.1",
+      },
+    };
+
+    const response = await fetch(postDataUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqData),
     });
     const res = await response.json();
     if (res) {
@@ -21,7 +75,7 @@ function App() {
 
   return (
     <div className="App">
-      <button style={{ fontSize: "28px", marginTop: "50px" }} onClick={postData}>
+      <button style={{ fontSize: "28px", marginTop: "50px" }} onClick={getKey}>
         Make Payment
       </button>
     </div>
