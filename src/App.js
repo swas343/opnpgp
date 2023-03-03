9f0f1fbf923dc117f7ed452586be60bdbf40dc9b
swastik sharma <swastik.343@gmail.com>
2023-03-03 10:14:03 +0530

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import { readMessage, decrypt, encrypt } from 'openpgp';
+import "./App.css";
+import { readMessage, decrypt, encrypt } from "openpgp";
+import useState from "react";
 
 function App() {
-  
+  const [data, setData] = useState(null);
+
+  async function postData() {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const res = await response.json();
+    if (res) {
+      console.log(res);
+    }
+  }
+
   return (
     <div className="App">
-      <button style={{fontSize:"28px", marginTop:"50px"}}>Make Payment</button>
+      <button style={{ fontSize: "28px", marginTop: "50px" }} onClick={postData}>
+        Make Payment
+      </button>
     </div>
   );
 }
